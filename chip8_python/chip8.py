8f46184c5f931d6ea3faab2ffcb4dbcd89339437
doleksiyenko <dan.oleksiyenko@gmail.com>
2024-05-18 22:21:11 -0400

    define font in memory class, add basic structure to renderer and chip8 file

---
+++ chip8_python/chip8.py
@@ -0,0 +1,6 @@
+from renderer import Renderer
+from memory import Memory
+
+if __name__ == "__main__":
+    memory = Memory()
+    renderer = Renderer()
--- chip8_python/renderer.py
+++ chip8_python/renderer.py
@@ -1,3 +1,8 @@
-"""
-A class for handling the graphics output of the CHIP-8
-"""
+import pygame
+
+class Renderer:
+    """
+    A class for handling the graphics output of the CHIP-8
+    """
+    def __init__(self):
+        pygame.init()
