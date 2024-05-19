79a46afc4e2e7af270ab2809860572a93ebc9897
doleksiyenko <dan.oleksiyenko@gmail.com>
2024-05-18 20:05:24 -0400

    Memory class structure

--- chip8_python/memory.py
+++ chip8_python/memory.py
@@ -1,3 +1,23 @@
-"""
-Class representing memory of the CHIP-8, and all associated read/write operations
-"""
+class Memory:
+    """
+    Class representing memory of the CHIP-8, and all associated read/write operations
+
+    All memory on the Chip-8 system is writeable.
+    """
+    def __init__(self):
+        # we want to store 4Kb of data, assume that each int can store at
+        # most 1 byte of info.
+        self.memory = [0] * 4096
+    
+    def load_program(self, ROM):
+        """
+        A Chip-8 program is loaded into memory starting at address 200 
+        """
+        pass
+
+    def load_interpreter(self, interpreter):
+        pass
+
+
+    def __load_file(self, file, memory_location):
+        pass
