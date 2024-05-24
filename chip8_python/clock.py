d8291def3762296c99f6a7949cd8e6e959289884
Daniel Oleksiyenko <51185857+doleksiyenko@users.noreply.github.com>
2024-05-24 11:09:30 -0400

    create clock class

---
+++ chip8_python/clock.py
@@ -0,0 +1,7 @@
+class Clock:
+    """
+    Clock class creates a clock with a specified framerate. Whenever the Clock method tick is called
+    check if enough time has passed to run the next frame, otherwise delay.
+    """
+    def __init__(self):
+        pass
