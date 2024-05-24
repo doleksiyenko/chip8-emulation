1030b6e7da678ef6f4dff3ebf0cd9525a648dbad
Daniel Oleksiyenko <51185857+doleksiyenko@users.noreply.github.com>
2024-05-24 11:59:07 -0400

    clock with working functionality

--- chip8_python/chip8.py
+++ chip8_python/chip8.py
@@ -1,8 +1,14 @@
 from renderer import Renderer
 from memory import Memory
+from clock import Clock
 
 if __name__ == "__main__":
     memory = Memory()
-    renderer = Renderer()
+    # renderer = Renderer()
 
-    renderer.quit()
+    clock = Clock(1)
+    for i in range(10):
+        print(i)
+        clock.tick()
+
+    # renderer.quit()
--- chip8_python/clock.py
+++ chip8_python/clock.py
@@ -1,7 +1,20 @@
+import time
+import typing
+
 class Clock:
     """
     Clock class creates a clock with a specified framerate. Whenever the Clock method tick is called
     check if enough time has passed to run the next frame, otherwise delay.
     """
-    def __init__(self):
-        pass
+    def __init__(self, frame_rate: int) -> None:
+        self.frame_rate = 1 / frame_rate 
+        self.prev_time = time.time()
+    
+    def tick(self) -> None:
+        # check the current time vs the previous time, if enough time has passed since the last frame, 
+        # then return
+        if time.time() - self.prev_time < self.frame_rate:
+            # wait
+            time.sleep(self.frame_rate - (time.time() - self.prev_time)) 
+
+        self.prev_time = time.time()
