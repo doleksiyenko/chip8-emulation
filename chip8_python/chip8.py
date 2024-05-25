ba6b2e55cf7e302fe20140642bb5412780afda73
doleksiyenko <dan.oleksiyenko@gmail.com>
2024-05-25 13:54:10 -0400

    create chip8 class

--- chip8_python/chip8.py
+++ chip8_python/chip8.py
@@ -1,13 +1,31 @@
 from renderer import Renderer
 from memory import Memory
 from clock import Clock
+from cpu import CPU
+from sound import Sound
 
-if __name__ == "__main__":
-    memory = Memory()
-    renderer = Renderer()
-    clock = Clock(1)
-    for i in range(10):
-        print(i)
-        clock.tick()
+class Chip8:
+    def __init__(self):
+        self.memory = Memory()
+        self.renderer = Renderer()
+        self.sound = Sound()
 
-    renderer.quit()
+        # run the CPU at 700 instructions per second
+        self.clock = Clock(rate=700)
+
+        self.cpu = CPU()
+        self.running = False
+
+
+    def start(self):
+        """
+        Enter the ain Fetch / Decode / Execute loop of the Chip8
+        """
+        self.running = True
+
+        while self.running:
+            self.close()
+
+    def close(self):
+        self.renderer.quit()
+        self.running = False
--- chip8_python/sound.py
+++ chip8_python/sound.py
@@ -1,3 +1,7 @@
 """
 A class for handling the sound output of the CHIP-8
 """
+
+class Sound:
+    def __init__(self):
+        pass
