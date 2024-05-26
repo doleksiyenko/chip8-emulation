bff6031560fcdfde2100bc5afe352d912c50d652
doleksiyenko <dan.oleksiyenko@gmail.com>
2024-05-26 11:16:58 -0400

    renderer - get and set pixel value

--- chip8_python/chip8.py
+++ chip8_python/chip8.py
@@ -15,7 +15,7 @@ class Chip8:
         # run the CPU at 700 instructions per second
         self.clock = Clock(rate=700)
 
-        self.cpu = CPU(memory=self.memory)
+        self.cpu = CPU(memory=self.memory, renderer=self.renderer)
         self.running = False
 
     def start(self):
@@ -23,22 +23,11 @@ class Chip8:
         Enter the main Fetch / Decode / Execute loop of the Chip8
         """
         self.running = True
-
         # set the screen to black to start with
         self.renderer.clear_screen()
 
+        self.renderer.render()
         while self.running:
-            # fetch instruction from memory
-            pc = self.cpu.get_pc()
-
-            # each instruction is 2 bytes long, get the first and second bytes
-            instruction_b1 = self.memory.get_instruction(loc=pc)
-            instruction_b2 = self.memory.get_instruction(loc=pc + 1)
-
-            # increment the program counter by 2
-            self.cpu.set_pc(pc + 2)
-
-
 
             # if the 'x' is pressed on the window, close the emulator 
             for event in pygame.event.get():
--- chip8_python/renderer.py
+++ chip8_python/renderer.py
@@ -9,21 +9,39 @@ class Renderer:
         
         # initialize the pygame window
         self.display = pygame.display.set_mode((64, 32)) 
-        
-    def clear_screen(self):
+        self.white = (255, 255, 255)
+        self.black = (0, 0, 0)
+
+    def clear_screen(self) -> None:
         self.display.fill('black')
+
+    def set_color_at_pixel(self, row: int, col: int, val: bool) -> None:
+        """
+        Set the pixel at position <row, col> to white if val is True, and black
+        if val is False.
+        """
+        if val:
+            self.display.set_at((col, row), 'white')
+        else:
+            self.display.set_at((col, row), 'black')
+
+    def get_color_at_pixel(self, row: int, col: int) -> bool:
+        """
+        Return 0 if the display is black and 1 if the display is white
+        at the given pixel at location <row, col>
+        """
+        return True if self.display.get_at((row, col)) == self.white else False
+
+
+    def render(self):
+        """
+        Update the whole display. This should be called at a given frame rate
+        (i.e. 60 Hz) and not at every CPU clock cycle.
+        """
         pygame.display.flip()
 
-    def set_color_at_pixel(self, row: int, col: int, val: bool):
-        """
-        Set the pixel at position <row, col> to on/off depending on the value of
-        <val>
-        """
-        pass
-
-    def get_color_at_pixel(self, row: int, col: int):
-        pass 
-
     def quit(self):
-        # quit pygame
+        """
+        Close the renderer (quit pygame)
+        """
         pygame.quit()
