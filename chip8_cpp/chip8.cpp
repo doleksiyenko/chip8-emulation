ab7b8f4d2d76ca1797c7b7d8cee5fb83de236711
doleksiyenko <dan.oleksiyenko@gmail.com>
2024-06-03 20:06:09 -0400

    create an empty SDL window for 5 seconds

---
+++ chip8_cpp/chip8.cpp
@@ -0,0 +1,18 @@
+#include <SDL2/SDL.h>
+#include <iostream>
+
+int main()
+{
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cout << "Error: "  << SDL_GetError();
+    }
+    else {
+        SDL_Window* window = SDL_CreateWindow("CHIP-8", 0, 0, 800, 600, SDL_WINDOW_SHOWN);
+        SDL_Delay(5000);
+        SDL_DestroyWindow(window);
+    }
+    
+    SDL_Quit();
+
+    return 0;
+}
