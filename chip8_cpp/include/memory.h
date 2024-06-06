5522ee303d511996197df65cd3bae61c70399697
Daniel Oleksiyenko <51185857+doleksiyenko@users.noreply.github.com>
2024-06-06 19:46:55 -0400

    memory class defintion in header

--- chip8_cpp/include/memory.h
+++ chip8_cpp/include/memory.h
@@ -1,9 +1,18 @@
 #ifndef MEMORY_H
 #define MEMORY_H
 
-class Memory {
-    Memory();
+using namespace std;
 
+
+class Memory {
+    public:
+        Memory();
+        void load_ROM(std::string file_path);
+        int get_from_memory(int memory_loc);
+        void set_memory(int memory_loc);
+
+    private:
+        unsigned char* memory;
 };
 
 #endif
