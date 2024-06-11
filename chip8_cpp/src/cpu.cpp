33fb48f38de6ac15958007c59f034ef8357e2351
doleksiyenko <dan.oleksiyenko@gmail.com>
2024-06-10 22:28:07 -0400

    draw instruction without rendering logic

--- chip8_cpp/src/cpu.cpp
+++ chip8_cpp/src/cpu.cpp
@@ -1,9 +1,11 @@
+#include <climits>
 #include <cpu.h>
 #include <cstdint>
 #include <iostream>
 #include <stack>
     
 #include <memory.h>
+#include <renderer.h>
 
 CPU::CPU(Memory* chip8_memory, Renderer* chip8_renderer) {
     pc = 0x200; // start the program counter at the beginning of the loaded ROM
@@ -75,6 +77,43 @@ void CPU::decode_execute(uint16_t instruction) {
         case 0xc000:
             break; 
         case 0xd000:
+            {
+                // draw to display instruction
+                // get the X and Y coordinates from the VX and VY registers
+                uint8_t vx = var_registers[(instruction & 0x0f00) >> 8] % SCREEN_WIDTH; 
+                uint8_t vy = var_registers[(instruction & 0x00f0) >> 4] % SCREEN_HEIGHT; 
+
+                // set the VF register to 0
+                var_registers[0xf] = 0x0;
+
+                // cap the columns/rows to be drawn if the rows/cols exceed the screen size
+                uint8_t row_len = 8;
+                if (row_len + vx >= SCREEN_WIDTH) {
+                    row_len = SCREEN_WIDTH - vx;
+                }
+
+                // sprite to be drawn has N bytes of data col_len = N
+                uint8_t col_len = instruction & 0x000f;
+                if (col_len + vy >= SCREEN_HEIGHT) {
+                    col_len = SCREEN_HEIGHT - vy;
+                }
+
+                // for every possible column val (every byte of the sprite)
+                for (int offset = 0; offset < col_len; offset++){
+                    uint8_t sprite_byte = chip8_memory->get_from_memory(i_register + offset);
+                    // increment through each bit of byte of sprite data
+                    for (int bit = 0; bit < row_len; bit++) {
+                        // get one bit at a time, starting from the MSB
+                        uint8_t sprite_bit = sprite_byte & (1 << (7 - bit)) >> (7 - bit); 
+                        uint8_t y = vy + offset; // y coordinate on display
+                        uint8_t x = vx + bit; // x coordinate on display
+
+                        if (sprite_bit == 1) {
+                                // TODO: implement rendering logic based on colour of the pixel
+                        }
+                    }
+                }
+            }
             break; 
         case 0xe000:
             break;
